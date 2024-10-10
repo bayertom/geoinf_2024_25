acdbe0ca367598df90c35b01bfd09073037a3300
bayertom <bayertom@natur.cuni.cz>
2024-10-10 11:07:04 +0200

    initial commit

---
+++ cv1/cv1.py
@@ -0,0 +1,31 @@
+from math import *
+#Coefficients of quadratic polynomial 
+a=6
+b=5
+c=1
+
+#Discriminant calculation
+D = b**2 - 4*a*c
+
+#Counting roots x1, x2, if D > 0
+if D > 0: 
+    x1 = (-b + D**(1/2))/(2*a)
+    x2 = (-b - sqrt(D))/(2*a)
+    print("Two roots, x1=",x1,"x2=",x2)
+    
+#Counting roots x1 = x2, if D == 0
+elif D == 0:
+    x1 = (-b)/(2*a)
+    print("One root, x1=x2",x1)
+
+#Counting roots x1 = x2, if D < 0
+else:
+    print("no solution in R")
+    
+#Compute factorial
+n = 100
+fn = 1
+while n>1:
+    fn=fn*n
+    n=n-1
+print("n! =", fn)
